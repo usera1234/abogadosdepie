bb0ee49d7d71fa56f32ef74ccaabde8fb353f275
Juan <chojuandelospalotes@gmail.com>
2025-10-09 14:23:23 -0300

    quienes somos grid voceto

--- paginaEquipo/components/app-equipo-list.js
+++ paginaEquipo/components/app-equipo-list.js
@@ -1,4 +1,7 @@
 import './equipo/list-socios.js';
+import './equipo/list-asociados.js';
+import './equipo/list-colaboradores.js';
+import './equipo/list-staff.js';
 
 class AppEquipoList extends HTMLElement {
     constructor() {
@@ -6,17 +9,58 @@ class AppEquipoList extends HTMLElement {
         this.innerHTML = `
             <div class="equipoListContainer">
                 <div class="equipoListNav">
-                    <button class="equipoListNavItem">Socios</button>
-                    <button class="equipoListNavItem">Asociados</button>
-                    <button class="equipoListNavItem">Colaboradores</button>
-                    <button class="equipoListNavItem">Staff</button>
+                    <button id="btn-socios" class="equipoListNavItem">Socios</button>
+                    <button id="btn-asociados" class="equipoListNavItem">Asociados</button>
+                    <button id="btn-colaboradores" class="equipoListNavItem">Colaboradores</button>
+                    <button id="btn-staff" class="equipoListNavItem">Staff</button>
+                </div>
+                <div id="list-container">
                 </div>
             </div>
         `
     }
+    connectedCallback() {
+        const listContainer = this.querySelector('#list-container');
+        const sociosBtn = this.querySelector('#btn-socios');
+        const asociadosBtn = this.querySelector('#btn-asociados');
+        const colaboradoresBtn = this.querySelector('#btn-colaboradores');
+        const staffBtn = this.querySelector('#btn-staff');    
+
+        sociosBtn.addEventListener('click', () => {
+            // Cuando se hace clic:
+            // a. Limpiamos el área de contenido.
+            listContainer.innerHTML = '';
+            // b. Insertamos el componente <list-socios> completo.
+            listContainer.innerHTML = '<list-socios></list-socios>';
+        });
+
+        asociadosBtn.addEventListener('click', () => {
+            // Cuando se hace clic:
+            // a. Limpiamos el área de contenido.
+            listContainer.innerHTML = '';
+            // b. Insertamos el componente <list-socios> completo.
+            listContainer.innerHTML = '<list-asociados></list-asociados>';
+        });
+
+        colaboradoresBtn.addEventListener('click', () => {
+            // Cuando se hace clic:
+            // a. Limpiamos el área de contenido.
+            listContainer.innerHTML = '';
+            // b. Insertamos el componente <list-socios> completo.
+            listContainer.innerHTML = '<list-colaboradores></list-colaboradores>';
+        });
+
+        staffBtn.addEventListener('click', () => {
+            // Cuando se hace clic:
+            // a. Limpiamos el área de contenido.
+            listContainer.innerHTML = '';
+            // b. Insertamos el componente <list-socios> completo.
+            listContainer.innerHTML = '<list-staff></list-staff>';
+        });
+        
+
+    }
 }
-
-
 
 customElements.define('app-equipo-list', AppEquipoList);
             
--- paginaEquipo/components/equipo/list-colaboradores.js
+++ paginaEquipo/components/equipo/list-colaboradores.js
@@ -1,4 +1,4 @@
-class AppListSocios extends HTMLElement {
+class AppListColaboradores extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
@@ -6,43 +6,31 @@ class AppListSocios extends HTMLElement {
                 <div class="listEquipoRow">
                     <div class="listEquipoItem">
                         <img src="/static/imagenes/equipo/socios/ezequielD.jpg" alt="Ezzequiel Dominguez" class="listEquipoImg">
-                        <div class="listEquipoTitle">Marcelo Ezequiel<br>Dominguez</div>
+                        <div class="listEquipoTitle">Marcelo Benajmin Dominguez</div>
                         <div class="listEquipoRedes">
                             <div class="listEquipoRedesRow">
                                 <img src="/static/imagenes/gmail-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
                                 <p class="listEquipoRedesText">mezequieldominguez@gmail.com</p>
-                            </div>
-                            <div class="listEquipoRedesRow">
-                                <img src="/static/imagenes/linkedin-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
-                                <a href="https://www.linkedin.com/in/marcelo-ezequiel-dominguez-4b4b9820b/" target="_blank" class="listEquipoRedesText">LinkedIn</a>
                             </div>
                         </div>
                     </div>
                     <div class="listEquipoItem">
                         <img src="/static/imagenes/equipo/socios/ezequielD.jpg" alt="Ezzequiel Dominguez" class="listEquipoImg">
-                        <div class="listEquipoTitle">Marcelo Juan<br>Dominguez</div>
+                        <div class="listEquipoTitle">Marcelo Juan Dominguez</div>
                         <div class="listEquipoRedes">
                             <div class="listEquipoRedesRow">
                                 <img src="/static/imagenes/gmail-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
                                 <p class="listEquipoRedesText">mezequieldominguez@gmail.com</p>
-                            </div>
-                            <div class="listEquipoRedesRow">
-                                <img src="/static/imagenes/linkedin-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
-                                <a href="https://www.linkedin.com/in/marcelo-ezequiel-dominguez-4b4b9820b/" target="_blank" class="listEquipoRedesText">LinkedIn</a>
                             </div>
                         </div>
                     </div>
                     <div class="listEquipoItem">
                         <img src="/static/imagenes/equipo/socios/ezequielD.jpg" alt="Ezzequiel Dominguez" class="listEquipoImg">
-                        <div class="listEquipoTitle">Marcelo Juan<br>Dominguez</div>
+                        <div class="listEquipoTitle">Marcelo Juan Dominguez</div>
                         <div class="listEquipoRedes">
                             <div class="listEquipoRedesRow">
                                 <img src="/static/imagenes/gmail-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
                                 <p class="listEquipoRedesText">mezequieldominguez@gmail.com</p>
-                            </div>
-                            <div class="listEquipoRedesRow">
-                                <img src="/static/imagenes/linkedin-equipo.png" alt="LinkedIn" class="listEquipoRedesImg">
-                                <a href="https://www.linkedin.com/in/marcelo-ezequiel-dominguez-4b4b9820b/" target="_blank" class="listEquipoRedesText">LinkedIn</a>
                             </div>
                         </div>
                     </div>
@@ -51,4 +39,4 @@ class AppListSocios extends HTMLElement {
         `
     }
 }
-customElements.define('list-socios', AppListSocios);    
+customElements.define('list-colaboradores', AppListColaboradores);    
