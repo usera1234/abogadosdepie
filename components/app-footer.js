010b84fe6d4a103db65aecabbbbcc6dcb4fa1a84
Juan <chojuandelospalotes@gmail.com>
2025-10-14 15:49:35 -0300

    quienes somos responsive

--- components/app-footer.js
+++ components/app-footer.js
@@ -6,27 +6,27 @@ class AppFooter extends HTMLElement {
             <div class="hor-bar" style="height: 2px; width: 100%"></div>                                         <!--Linea horizontal divisora-->
 
             <div id="footer" class="footer-container">                                                              <!--Pie de pagina-->
-                <div class="container-row" style="gap:12%; align-items: center;">
-                    <div class="container-row">                                                                      <!--Logo pie de pagina-->
+                <div class="footer-row">
+                    <div class="footer-row">                                                                      <!--Logo pie de pagina-->
                     <img class="logo" src="static/imagenes/10a_logo_adp.png" alt="logo abogados de pie" style="padding-left:30px;height:100px; width: 300px"> <!--Logo pie de pagina-->
                     </div>
-                    <div class="container-row" style="gap:20rem;">                                                  <!--Botones redes-->
-                    <div class="container-row" style="gap:2rem;">
-                        <div id="twitter" class="container-row">                                                    <!--Twitter-->
+                    <div class="footer-row" style="gap:20rem;">                                                  <!--Botones redes-->
+                    <div class="footer-row" style="gap:2rem;">
+                        <div id="twitter" class="footer-row">                                                    <!--Twitter-->
                         <a href="https://x.com/AbogadosDePie?t=Abz6bWPfU0G38n1BWFskcQ&s=08" target="_blank">
                             <button class="btn" style="width:100px; height:100px; background-color: white;">
                             <img class="logo" src="static/imagenes/twt.png" style="height:30px; width:40px;"> 
                             </button>
                         </a>
                         </div>
-                        <div id="Facebook" class="container-row">                                                   <!--Facebook-->
+                        <div id="Facebook" class="footer-row">                                                   <!--Facebook-->
                         <a href="https://www.facebook.com/people/Abogados-de-Pie/100080766828964/?mibextid=ZbWKwL" target="_blank">
                             <button class="btn" style="width:100px; height:100px; background-color: white;">
                             <img class="logo" src="static/imagenes/fcb.png" style="height: 50px; width: 50px;"> 
                             </button>
                         </a>
                         </div>
-                        <div id="instagram" class="container-row">                                                  <!--Instagram-->
+                        <div id="instagram" class="footer-row">                                                  <!--Instagram-->
                         <a href="https://www.instagram.com/abogadosdepie.ok/" target="_blank">
                             <button  class="btn" style="width:100px; height:100px; background-color: white;">
                             <img class="logo" src="static/imagenes/ig.png" style="height: 50px; width: 50px;"> 
